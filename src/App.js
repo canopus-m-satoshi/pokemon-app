e149f605606b34015559a4a491db1946ed99bfa4
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-05 23:43:10 +0900

    fetch pokemon API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import './App.css'
+import { getAllPokemon } from './utils/pokemon'
 
 function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL)
+      console.log('🚀 ~ file: App.js:10 ~ fetchPokemonData ~ res', res)
+    }
+
+    fetchPokemonData()
+  }, [])
+
   return <div className="App"></div>
 }
 
