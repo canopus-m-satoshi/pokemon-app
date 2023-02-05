23258e77a05341f7255e8316c096b936ebdccfcc
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-05 23:47:53 +0900

    loading func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { getAllPokemon } from './utils/pokemon'
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -12,9 +14,14 @@ function App() {
     }
 
     fetchPokemonData()
+    setIsLoading(false)
   }, [])
 
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      {isLoading ? <h1>読み込み中です</h1> : <h1>読み込み終了しました</h1>}
+    </div>
+  )
 }
 
 export default App
