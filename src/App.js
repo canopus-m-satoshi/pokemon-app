22bd43b30df135b5225574cd007e9ab31d35a492
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-12 00:15:29 +0900

    added Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 import { getAllPokemon, getPokemon } from './utils/pokemon'
 
 function App() {
@@ -31,21 +32,24 @@ function App() {
   }, [])
 
   return (
-    <div className="w-full h-full bg-blue-200">
-      <div className="py-4 px-5 container mx-auto">
-        {isLoading ? (
-          <h1>読み込み中です</h1>
-        ) : (
-          <>
-            <div className="text-center container grid  gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {pokemonData.map((pokemon, i) => {
-                return <Card key={i} pokemon={pokemon} />
-              })}
-            </div>
-          </>
-        )}
+    <>
+      <div className="w-full h-full bg-blue-200">
+        <Navbar />
+        <div className="py-4 px-5 container mx-auto">
+          {isLoading ? (
+            <h1>読み込み中です</h1>
+          ) : (
+            <>
+              <div className="text-center container grid  gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {pokemonData.map((pokemon, i) => {
+                  return <Card key={i} pokemon={pokemon} />
+                })}
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
