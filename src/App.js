d1984836250e5ad746265f8028c29ea45e897dd7
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-02-12 00:09:20 +0900

    styleing card component

--- src/App.js
+++ src/App.js
@@ -31,18 +31,20 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {isLoading ? (
-        <h1>読み込み中です</h1>
-      ) : (
-        <>
-          <div className="pokemonCardContainer">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />
-            })}
-          </div>
-        </>
-      )}
+    <div className="w-full h-full bg-blue-200">
+      <div className="py-4 px-5 container mx-auto">
+        {isLoading ? (
+          <h1>読み込み中です</h1>
+        ) : (
+          <>
+            <div className="text-center container grid  gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,31 +2,34 @@ const Card = ({ pokemon }) => {
   const { name, sprites, types, weight, height, abilities } = pokemon
   console.log('🚀 ~ file: Card.js:2 ~ Card ~ pokemon', pokemon)
   return (
-    <div className="card">
-      <div className="cardImg">
-        <img src={sprites.front_default} alt="" />
-        <h3 className="cardName">{name}</h3>
-
-        <div className="cardTypes">
-          {types.map((type, i) => {
-            return (
-              <div key={i}>
-                <div>Type:</div>
-                <span className="typeName">{type.type.name}</span>
-              </div>
-            )
-          })}
-        </div>
+    <div className="border drop-shadow-lg bg-white py-2 px-5">
+      <div>
+        <img src={sprites.front_default} alt={name} className="mx-auto" />
       </div>
-      <div className="cardInfo">
-        <div className="cardData">
-          <p className="title">Weight: {weight} gram</p>
+
+      <h3 className="text-xl font-bold">{name.toUpperCase()}</h3>
+
+      <div className="mt-5 grid gap-1 place-items-center md:place-items-start">
+        <div className="flex items-start">
+          <div className="">Type:</div>
+          <div className="flex flex-col items-start ml-1">
+            {types.map((type, i) => {
+              return (
+                <div key={i}>
+                  <span className="typeName">{type.type.name}</span>
+                </div>
+              )
+            })}
+          </div>
         </div>
-        <div className="cardData">
-          <p className="title">Height: {height} cm</p>
+        <div>
+          <p>Weight: {weight} gram</p>
         </div>
-        <div className="cardData">
-          <p className="title">Abilities: {abilities[0].ability.name}</p>
+        <div>
+          <p>Height: {height} cm</p>
+        </div>
+        <div>
+          <p>Abilities: {abilities[0].ability.name}</p>
         </div>
       </div>
     </div>
